9bc38376ed64eea9a15b78272b7896b743fff25a
andrewtio <andrewprasetio@gmail.com>
2024-10-25 21:19:23 +0700

    exercise 11.9 final fix port

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 describe("Pokedex", function () {
   it("front page can be opened", function () {
-    cy.visit("http://localhost:8080")
+    cy.visit("http://localhost:5000")
     cy.contains("ivysaur")
     cy.contains(
       "Pokémon and Pokémon character names are trademarks of Nintendo."
@@ -9,7 +9,7 @@ describe("Pokedex", function () {
   })
 
   it("pokemon page can be navigated to", function () {
-    cy.visit("http://localhost:8080")
+    cy.visit("http://localhost:5000")
     cy.contains("ivysaur").click()
     cy.contains("overgrow")
     cy.contains("chlorophyll")

eef56a90dc8ee19cde8021eb39b80133ed31b60e
andrewtio <andrewprasetio@gmail.com>
2024-11-08 20:47:05 +0700

    test error deployment

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 8080
 
-app.use(express.static("dist"))
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  this_causes_error
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })

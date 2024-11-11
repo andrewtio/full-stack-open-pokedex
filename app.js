a0f3e330e545d226d4604a7cc8b670794299874c
andrewtio <andrewprasetio@gmail.com>
2024-11-11 20:13:28 +0700

    exercise 11.12

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 8080
 
-app.use(express.static('dist'))
+app.use(express.static("dist"))
 
-app.listen(PORT, () => {
-  this_causes_error
+app.get("/health", (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw("error...  ")
+  res.send("ok")
+})
+
+app.listen(PORT + 1, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
